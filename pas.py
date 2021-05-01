f3657f08d991154bc34ced79f3792c29c63df237
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 07:04:54 +0800

    Create pas.py

---
+++ pas.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+s=requests.Session()
+s.verify=False
+requests.packages.urllib3.disable_warnings()
+
+def pas(host,pw):
+  url='https://'+host
+  with s.get(url) as web:
+    text=web.text
+    soup=BeautifulSoup(text,features='lxml')
+    csrf=soup.find('input',{'name':'csrf'}).get('value')
+    ip=soup.find('input',{'name':'ip'}).get('value')
+  with s.post(url,data={'pw':pw,'csrf':csrf,'ip':ip}) as web:
+    print(web.request.body)
+    text=web.text
+    soup=BeautifulSoup(text,features='lxml')
+    notice=soup.find('div',{'class':'notice'}).string
+    notice=notice.strip()
+    print(notice)

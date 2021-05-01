220ac6c500c07605d4f7e6049e14ce9777525cde
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 07:51:13 +0800

    Update ba.py

--- ba.py
+++ ba.py
@@ -1,13 +1,16 @@
 import requests
 import time
 import os
+import pas
 
 name='苦力怕签到'
 pw=os.getenv('talk_pw')
 host=os.getenv('host')
+frppw=os.getenv('pw')
 
 def send(content):
   global name,pw
+  pas.pas(host.replace('http://','https://'),frppw)
   name="<font color='5BA783'>"+name+'</font>'
   ticks=str(int(time.time()))
   file={'username':(None,name),'file':('captcha_'+ticks,content)}

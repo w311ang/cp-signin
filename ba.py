4387f8da4cfddb0e5308256e3888fbf2e5ed7f6f
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-01 23:19:07 +0800

    Update ba.py

--- ba.py
+++ ba.py
@@ -4,13 +4,14 @@ import os
 
 name='苦力怕签到'
 pw=os.getenv('talk_pw')
+host=os.getenv('host')
 
 def send(content):
   global name,pw
   name="<font color='5BA783'>"+name+'</font>'
   ticks=str(int(time.time()))
   file={'username':(None,name),'file':('captcha_'+ticks,content)}
-  re=requests.post('http://raspberrypi.lan/Meow-Chat/image.php',files=file).text
+  re=requests.post(host+'/Meow-Chat/image.php',files=file).text
   print(re)
-  re=requests.get('http://raspberrypi.lan/Meow-Chat/write.php?password=%s&message=%s&username=%s'%(pw,'请输入验证码',name)).text
+  re=requests.get(host+'/Meow-Chat/write.php?password=%s&message=%s&username=%s'%(pw,'请输入验证码',name)).text
   print(re)

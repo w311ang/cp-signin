3476826577eba90fcf9a3f7d265bfdebbf915d94
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-01 23:00:54 +0800

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+def receive(ignore):
+  notfound=True
+  while notfound:
+    re=requests.get('http://raspberrypi.lan/Meow-Chat/read.php').text
+    soup=BeautifulSoup(re,features="lxml")
+    talk=soup.find_all('div')[-3].find_all('font')[-1].string
+    if talk:
+      talk=talk.rstrip()
+      #print(talk,talk==ignore)
+      if not talk==ignore:
+        notfound=False
+  return talk

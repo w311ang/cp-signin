7b241338566c85500d013507d5d268b37cdf1a1a
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 16:51:31 +0800

    Update pas.py

--- pas.py
+++ pas.py
@@ -4,6 +4,7 @@ import requests
 s=requests.Session()
 s.verify=False
 requests.packages.urllib3.disable_warnings()
+psssed=[]
 
 def pas(host,pw):
   if 'http://' or 'https://' in host:
@@ -11,21 +12,24 @@ def pas(host,pw):
   else:
     url='https://'+host
   #print(host)
-  try:
-    with s.get(url) as web:
-      text=web.text
-      soup=BeautifulSoup(text,features='lxml')
-      csrf=soup.find('input',{'name':'csrf'}).get('value')
-      ip=soup.find('input',{'name':'ip'}).get('value')
-    with s.post(url,data={'pw':pw,'csrf':csrf,'ip':ip}) as web:
-      #print(web.request.body)
-      text=web.text
-      soup=BeautifulSoup(text,features='lxml')
-      notice=soup.find('div',{'class':'notice'}).string
-      notice=notice.strip()
-      print(notice)
-      return notice
-  except requests.exceptions.SSLError:
-    re='SSL出错，可能已验证'
-    print(re)
-    return re
+  if not url in passed:
+   try:
+      with s.get(url) as web:
+        text=web.text
+        soup=BeautifulSoup(text,features='lxml')
+        csrf=soup.find('input',{'name':'csrf'}).get('value')
+        ip=soup.find('input',{'name':'ip'}).get('value')
+      with s.post(url,data={'pw':pw,'csrf':csrf,'ip':ip}) as web:
+        #print(web.request.body)
+        text=web.text
+        soup=BeautifulSoup(text,features='lxml')
+        notice=soup.find('div',{'class':'notice'}).string
+        notice=notice.strip()
+        print(notice)
+        if '认证成功' in notice:
+          passed.append(url)
+        return notice
+    except requests.exceptions.SSLError:
+      re='SSL出错，可能已验证'
+      print(re)
+      return re
